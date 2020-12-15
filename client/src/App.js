7a737923c67bdeb6f11660413c6afc51ba1918e0
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2020-12-15 00:42:56 -0500

    fin going to start flex

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route, Link, Switch} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +17,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -24,13 +28,22 @@ export default function App () {
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
+    let movie = movieList.filter(movie => movie.id !== id)
+    console.log(movie)
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList list={saved} />
 
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route path='/movies/:id'>
+          <Movie movies={[]} />
+        </Route>
+        <Route path='/'>
+          <MovieList movies={movieList} />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
+
+
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
